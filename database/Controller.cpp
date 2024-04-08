3a3edea2682fe34565b32aab9b1363940557b465
Plamen Atanasov <plamen.atanasov25@gmail.com>
2024-04-08 08:01:27 +0300

    Add constructor for Controller and tables info vector

--- database/Controller.cpp
+++ database/Controller.cpp
@@ -1,15 +1,42 @@
 #include "Controller.h"
 
+#include <fstream>
+
 #include "Command.h"
 
 void Controller::createTable(const StringVector& args)
 {}
 
+void Controller::readTables()
+{
+	std::ifstream ifile(TABLES_FILE, std::ios::binary);
+	if (!ifile.is_open()) {
+		return;
+	}
+
+	StringPair temp;
+	size_t size;
+	ifile.read(reinterpret_cast<char*>(&size), sizeof(size));
+	tables.reserve(size);
+
+	for (size_t i = 0; i < size; i++) {
+		ifile.read(reinterpret_cast<char*>(&temp), sizeof(temp)); // would this work with std::string?
+		tables.push_back(temp);
+	}
+
+	ifile.close();
+}
+
+Controller::Controller()
+{
+	readTables();
+}
+
 void Controller::execute(const Command& command)
 {
 	switch (command.getCommandName()) 
 	{
-	case CommandName::CREATE:
+	case CommandType::CREATE:
 		createTable(command.getArgs());
 		break;
 	}
--- database/Controller.h
+++ database/Controller.h
@@ -7,10 +7,22 @@
 
 using StringVector = std::vector<std::string>;
 
+struct StringPair {
+	String tableName;
+	String fileName;
+};
+
 class Controller
 {
-private:
-	void createTable(const StringVector& args);
 public:
+	Controller();
+
 	void execute(const Command& command);
+private:
+	std::vector<StringPair> tables;
+
+	static constexpr const char* TABLES_FILE = "tables.dat"; //.dat or .txt for the tables file?
+
+	void createTable(const StringVector& args);
+	void readTables();
 };
