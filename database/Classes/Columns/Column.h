e375e546a9c428ecb48ed02d3619f736495c07e3
Plamen Atanasov <plamen.atanasov25@gmail.com>
2024-05-07 17:49:38 +0300

    Add Column base abstract class with methods for manipulating values

---
+++ database/Classes/Columns/Column.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+#include "../Helpers/ColumnType.h"
+
+class Column {
+public:
+	Column(const std::string& name, ColumnType type) : type(type) {
+		strcpy_s(this->name, name.c_str());
+	}
+	virtual ~Column() = default;
+
+	virtual void addValue(const std::string& val) = 0;
+	virtual void removeValue(int pos) = 0;
+	virtual void changeValue(int pos, const std::string& newVal) = 0;
+
+	virtual void printValues() const = 0;
+
+	virtual Column* clone() const = 0;
+private:
+	static constexpr int MAX_NAME_LENGTH = 33;
+
+	char name[MAX_NAME_LENGTH]{};
+	ColumnType type;
+};
