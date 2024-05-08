ab44ba6361db642567401a0ed1138c6c91d5d8ae
Plamen Atanasov <plamen.atanasov25@gmail.com>
2024-05-08 09:19:32 +0300

    Add exception in getColumn factory method when invalid type is passed

--- database/Classes/Helpers/ColumnFactory.cpp
+++ database/Classes/Helpers/ColumnFactory.cpp
@@ -4,7 +4,9 @@
 
 Column* getColumn(const std::string& name, ColumnType type) {
 	switch (type) {
-	case ColumnType::INT: return new IntColumn(name, type);
-	case ColumnType::COLUMN_TYPES_COUNT: throw std::invalid_argument("type is invalid");
+	case ColumnType::INT:
+		return new IntColumn(name, type);
 	}
+
+	throw std::invalid_argument("type is invalid");
 }
