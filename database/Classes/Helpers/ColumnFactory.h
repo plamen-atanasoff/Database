df3b550c4c48561f6bad077aff45af1b8c10ef84
Plamen Atanasov <plamen.atanasov25@gmail.com>
2024-05-08 09:40:03 +0300

    Move files

--- database/Classes/Helpers/ColumnFactory.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <string>
-#include "ColumnType.h"
-#include "../Columns/IntColumn.h"
-
-Column* getColumn(const std::string& name, ColumnType type);
--- database/Classes/Helpers/ColumnType.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-enum class ColumnType {
-	INT,
-
-	COLUMN_TYPES_COUNT
-};
